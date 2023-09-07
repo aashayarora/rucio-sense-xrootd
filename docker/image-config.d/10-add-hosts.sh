895381553673d058d2af097dbea4287114fabfbf
aaarora <aaarora@ucsd.edu>
2023-09-07 14:12:22 -0700

    add redi hosts, make symlinks

---
+++ docker/image-config.d/10-add-hosts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -v REDIRECTOR ]; then
+  echo 2001:48d0:3001:111::400 xrootd-sense-ucsd-01.sdsc.optiputer.net > /etc/hosts
+  echo 2001:48d0:3001:111::500 xrootd-sense-ucsd-02.sdsc.optiputer.net > /etc/hosts
+  echo 2001:48d0:3001:111::600 xrootd-sense-ucsd-03.sdsc.optiputer.net > /etc/hosts
+  echo 2001:48d0:3001:111::700 xrootd-sense-ucsd-04.sdsc.optiputer.net > /etc/hosts
+  
+  echo 2001:48d0:3001:112::400 xrootd-sense-ucsd-01-112.sdsc.optiputer.net > /etc/hosts
+  echo 2001:48d0:3001:112::500 xrootd-sense-ucsd-02-112.sdsc.optiputer.net > /etc/hosts
+  echo 2001:48d0:3001:112::600 xrootd-sense-ucsd-03-112.sdsc.optiputer.net > /etc/hosts
+  echo 2001:48d0:3001:112::700 xrootd-sense-ucsd-04-112.sdsc.optiputer.net > /etc/hosts
+fi
--- docker/image-config.d/60-make-files.sh
+++ docker/image-config.d/60-make-files.sh
@@ -2,15 +2,17 @@ if [ -n $NUM_FILES ]; then
   ## CREATE TEST FILES AND LINKS ##
   data_dir="/rucio/store/data/Run2018A/EGamma/MINIAOD/UL2018_MiniAODv2-v1/50000"
   num_files=$NUM_FILES
+  start_point=$START_POINT
 
   mkdir -p "$data_dir"
 
-  for i in $(seq 0 "$num_files"); do
-    file_name="testSourceFile$i"
-    dd if=/dev/zero of="/rucio/$file_name" bs=1G count=1
-    ln "/rucio/$file_name" "$data_dir/$file_name.root"
+  for ((i=start_point; i<=num_files+start_point; i++)); do
+    dd if=/dev/zero of="/rucio/testSourceFile$i" bs=1G count=1
+    for ((j=0; j<=4; j++)); do
+      ln "/rucio/testSourceFile$i" "${data_dir}/testSourceFile$((j * num_files + i)).root"
+    done
   done
-  
+
   # Set ownership to xrootd user and group
   chown -R xrootd:xrootd /rucio/*
 fi
