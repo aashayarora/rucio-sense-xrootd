d58bd7ee75e0ca3986ce044e5f8375213d52134c
aashayarora <aaarora@ucsd.edu>
2024-07-01 06:28:42 -0700

    update

--- docker/image-config.d/20-cp-certs.sh
+++ docker/image-config.d/20-cp-certs.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-                                                                                                                                                        
+
+# curl http://uaf-7.t2.ucsd.edu/~aaarora/certs/crl/ca.crt -o /etc/grid-security/certificates/RucioCA.pem
+# curl http://uaf-7.t2.ucsd.edu/~aaarora/certs/crl/ca.crl -o /etc/grid-security/certificates/560061af.r0
+# curl http://uaf-7.t2.ucsd.edu/~aaarora/certs/crl/ca.crt -o /etc/grid-security/certificates/560061af.0
+
+# chmod 644 /etc/grid-security/certificates/560061af.0
+# chown root: /etc/grid-security/certificates/560061af.0
+
+# chmod u+rx /etc/xrootd/xrdcp-tpc.sh
+# chown xrootd:xrootd /etc/xrootd/xrdcp-tpc.sh
+
 CERT_DIR=/etc/grid-security/
 mkdir -p $CERT_DIR/xrd
 if [ -f $CERT_DIR/hostcert.pem ]; then
--- docker/image-config.d/60-make-files.sh
+++ docker/image-config.d/60-make-files.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-# num_files="${NUM_FILES:-10}"
-# num_datasets="${NUM_DATASETS:-10}"
-# num_symlinks="${NUM_SYMLINKS:-10}"
+if [ -n "$MAKE_FILES" ]; then
 
-# if [ -z "$REDIRECTOR" ]; then
-#   data_dir="/rucio/store/data/Run2018A/EGamma/MINIAOD/UL2018_MiniAODv2-v1/"
-#   mkdir -p "$data_dir"
+num_files="${NUM_FILES:-10}"
+num_datasets="${NUM_DATASETS:-10}"
+num_symlinks="${NUM_SYMLINKS:-10}"
 
-#   for ((i=0; i<=num_files; i++)); do
-#     dd if=/dev/zero of="/rucio/testSourceFile$i" bs=1G count=4
-#     for ((d=65000; d<=65000+1000*num_datasets; d+=1000)); do
-#       for ((j=0; j<=num_symlinks; j++)); do
-#         mkdir "${data_dir}/$d"
-#         ln "/rucio/testSourceFile$i" "${data_dir}/$d/testSourceFile$((j * num_files + i)).root"
-#       done
-#     done
-#   done
-# fi
+base_dir="${BASE_DIR:-/rucio}"
 
-# # Set ownership to xrootd user and group
-# chown -R xrootd:xrootd /rucio/*
+data_dir="$base_dir/store/data/Run2018A/EGamma/MINIAOD/UL2018_MiniAODv2-v1"
+mkdir -p "$data_dir"
+
+for ((i=0; i<=num_files; i++)); do
+  dd if=/dev/zero of="$base_dir/testSourceFile$i" bs=1G count=4
+  for ((d=65000; d<=65000+1000*num_datasets; d+=1000)); do
+    mkdir -p "${data_dir}/$d"
+    for ((j=0; j<=num_symlinks; j++)); do
+      ln "$base_dir/testSourceFile$i" "${data_dir}/$d/testSourceFile$((j * num_files + i)).root"
+    done
+  done
+done
+
+fi
+
+# Set ownership to xrootd user and group
+chown -R xrootd:xrootd /rucio/*
+
