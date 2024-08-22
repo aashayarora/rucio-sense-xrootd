99246c2598263ae25bdc611edb7efab83212f2d7
aashayarora <aaarora@ucsd.edu>
2024-08-22 06:49:24 -0700

    update

--- docker/image-config.d/20-cp-certs.sh
+++ docker/image-config.d/20-cp-certs.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# curl http://uaf-7.t2.ucsd.edu/~aaarora/certs/crl/ca.crt -o /etc/grid-security/certificates/RucioCA.pem
-# curl http://uaf-7.t2.ucsd.edu/~aaarora/certs/crl/ca.crl -o /etc/grid-security/certificates/560061af.r0
-# curl http://uaf-7.t2.ucsd.edu/~aaarora/certs/crl/ca.crt -o /etc/grid-security/certificates/560061af.0
-
-# chmod 644 /etc/grid-security/certificates/560061af.0
-# chown root: /etc/grid-security/certificates/560061af.0
+curl http://uaf-2.t2.ucsd.edu/~aaarora/packages/560061af.0 -o /etc/grid-security/certificates/560061af.0
+chmod 644 /etc/grid-security/certificates/560061af.0
+chown root: /etc/grid-security/certificates/560061af.0
 
 # chmod u+rx /etc/xrootd/xrdcp-tpc.sh
 # chown xrootd:xrootd /etc/xrootd/xrdcp-tpc.sh
