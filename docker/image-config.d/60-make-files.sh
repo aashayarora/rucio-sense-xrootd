53431dada0e06d9d8d2931e62f746a796fe558c8
aashayarora <aaarora@ucsd.edu>
2025-06-26 13:38:13 -0700

    update

--- docker/image-config.d/60-make-files.sh
+++ docker/image-config.d/60-make-files.sh
@@ -11,11 +11,11 @@ base_dir="${BASE_DIR:-/rucio}"
 data_dir="$base_dir/store/data/Run2018A/EGamma/MINIAOD/UL2018_MiniAODv2-v1"
 mkdir -p "$data_dir"
 
-for ((i=0; i<=num_files; i++)); do
+for ((i=0; i<num_files; i++)); do
   dd if=/dev/zero of="$base_dir/testSourceFile$i" bs=1G count=4
-  for ((d=65000; d<=65000+1000*num_datasets; d+=1000)); do
+  for ((d=65000; d<65000+1000*num_datasets; d+=1000)); do
     mkdir -p "${data_dir}/$d"
-    for ((j=0; j<=num_symlinks; j++)); do
+    for ((j=0; j<num_symlinks; j++)); do
       ln "$base_dir/testSourceFile$i" "${data_dir}/$d/testSourceFile$((j * num_files + i)).root"
     done
   done
